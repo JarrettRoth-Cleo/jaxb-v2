6cc09760d111cea2808d32be5de663dff945a0a6
wgeiger <wgeiger@cleo.com>
2018-05-08 13:46:11 -0400

    Moved restriction tests into their own test suite

--- jaxb-ri/xjc/src/test/java/xjcTests/XJCRestrictionTests.java
+++ jaxb-ri/xjc/src/test/java/xjcTests/XJCRestrictionTests.java
@@ -1,5 +1,64 @@
 package xjcTests;
 
-public class XJCRestrictionTests {
+import java.io.File;
+import java.util.List;
 
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.sun.tools.xjc.Plugin;
+
+public class XJCRestrictionTests extends AbstractXJCTest{
+
+	//  @Ignore
+  @Test
+  public void restrictionIssue() throws Throwable{
+  	runTest(new Logic(){
+
+			@Override
+			public File getXsd() {
+				return new File(resourceDir,"JustRestrictionIssue.xsd");
+			}
+
+
+			@Override
+			public void loadBindings(List<File> files) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void loadPlugins(List<Plugin> plugins) {
+				// TODO Auto-generated method stub
+				
+			}
+  		
+  	});
+  }
+  
+  @Ignore
+  @Test
+  public void fullRestrictionIssue() throws Throwable{
+  	runTest(new Logic(){
+
+			@Override
+			public File getXsd() {
+				return new File("C:/code/xjcFork/xsds/RestrictionIssue","Party.xsd");
+			}
+
+
+			@Override
+			public void loadBindings(List<File> files) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void loadPlugins(List<Plugin> plugins) {
+				// TODO Auto-generated method stub
+				
+			}
+  		
+  	});
+  }  
 }
