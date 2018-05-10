212fb9ffb87be9b4d06d52ba669114a5330fd467
wgeiger <wgeiger@cleo.com>
2018-05-10 10:19:47 -0400

    Restriction resolution (#5)
    * Added a sample of the restriction issue for XJC testing
    
    * Added cleanup of testing resources at the end of XJCTests
    
    * Refactored XJCTests to use a shared interface.  Will be removing restriction examples from the XJCTests
    
    * Moved restriction tests into their own test suite
    
    * Refactored interface into abstract class so base implementation can be utilized
    
    * Added new test logic to restriction tests, set th Abstract extended complex type builder to ignore the restriction exception
    
    * Added information to the README about how to package changes for Clarify
    
    * Added more information to README and added any change to the Restriction issue xsd
    
    * Added more to the readme

---
+++ jaxb-ri/xjc/src/test/java/xjcTests/XJCTests.java
@@ -0,0 +1,59 @@
+package xjcTests;
+import java.io.File;
+import java.util.List;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class XJCTests extends AbstractXJCTest {
+
+    @Ignore
+    @Test
+    public void simpleTest() throws Throwable{
+        runTest(new Logic(){
+
+			@Override
+			public File getXsd() {
+				return (new File(resourceDir,"ImageAttachment.xsd"));
+			}    		
+    	});
+    }
+
+    
+    @Ignore
+    @Test
+    public void shouldFailTest() throws Throwable{
+        runTest(new Logic(){
+
+			@Override
+			public File getXsd() {
+				return new File(resourceDir,"EADS_INVOICING_JUST_PRECISION.XSD");
+			}
+
+    		
+    	});
+    }
+
+    @Ignore
+    @Test
+    public void shouldFailWithBindings3() throws Throwable{
+
+        runTest(new Logic(){
+
+			@Override
+			public File getXsd() {
+				return new File(resourceDir,"EADS_INVOICING_JUST_PRECISION.XSD");
+			}
+
+
+			@Override
+			public void loadBindings(List<File> files) {
+				files.add(new File(resourceDir,"Just_precision_bindings.xjb"));
+				
+			}
+
+    		
+    	});
+    }
+
+}
