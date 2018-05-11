d83aa9f23de349092c58c93a6c945595db7e6cfa
wgeiger <wgeiger@cleo.com>
2018-05-11 14:08:15 -0400

    Moved Defined name converters to provider class. This causes many errors that will be fixed

---
+++ jaxb-ri/core/src/main/java/com/sun/xml/bind/api/impl/NameConverterProvider.java
@@ -0,0 +1,234 @@
+package com.sun.xml.bind.api.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import javax.lang.model.SourceVersion;
+
+public class NameConverterProvider {
+
+	private static NameConverter nc;
+
+	public static void setStandardNameConverter(NameConverter nc) {
+		NameConverterProvider.nc = nc;
+	}
+
+	public static NameConverter getStandardNameConverter() {
+		if (nc == null) {
+			nc = new Standard();
+		}
+		return nc;
+	}
+
+	static class Standard extends NameUtil implements NameConverter {
+
+		public Standard() {
+			System.out.println("Standard created...");
+		}
+
+		@Override
+		public String toClassName(String s) {
+			return toMixedCaseName(toWordList(s), true);
+			// return s;
+		}
+
+		@Override
+		public String toVariableName(String s) {
+			// return toMixedCaseName(toWordList(s), false);
+			return s;
+		}
+
+		@Override
+		public String toInterfaceName(String token) {
+			return toClassName(token);
+		}
+
+		@Override
+		public String toPropertyName(String s) {
+			String prop = toClassName(s);
+			// property name "Class" with collide with Object.getClass,
+			// so escape this.
+			if (prop.equals("Class"))
+				prop = "Clazz";
+			return prop;
+		}
+
+		@Override
+		public String toConstantName(String token) {
+			return super.toConstantName(token);
+		}
+
+		/**
+		 * Computes a Java package name from a namespace URI, as specified in
+		 * the spec.
+		 *
+		 * @return null if it fails to derive a package name.
+		 */
+		@Override
+		public String toPackageName(String nsUri) {
+			// remove scheme and :, if present
+			// spec only requires us to remove 'http' and 'urn'...
+			int idx = nsUri.indexOf(':');
+			String scheme = "";
+			if (idx >= 0) {
+				scheme = nsUri.substring(0, idx);
+				if (scheme.equalsIgnoreCase("http") || scheme.equalsIgnoreCase("urn"))
+					nsUri = nsUri.substring(idx + 1);
+			}
+
+			// tokenize string
+			ArrayList<String> tokens = tokenize(nsUri, "/: ");
+			if (tokens.size() == 0) {
+				return null;
+			}
+
+			// remove trailing file type, if necessary
+			if (tokens.size() > 1) {
+				// for uri's like "www.foo.com" and "foo.com", there is no
+				// trailing
+				// file, so there's no need to look at the last '.' and
+				// substring
+				// otherwise, we loose the "com" (which would be wrong)
+				String lastToken = tokens.get(tokens.size() - 1);
+				idx = lastToken.lastIndexOf('.');
+				if (idx > 0) {
+					lastToken = lastToken.substring(0, idx);
+					tokens.set(tokens.size() - 1, lastToken);
+				}
+			}
+
+			// tokenize domain name and reverse. Also remove :port if it exists
+			String domain = tokens.get(0);
+			idx = domain.indexOf(':');
+			if (idx >= 0)
+				domain = domain.substring(0, idx);
+			ArrayList<String> r = reverse(tokenize(domain, scheme.equals("urn") ? ".-" : "."));
+			if (r.get(r.size() - 1).equalsIgnoreCase("www")) {
+				// remove leading www
+				r.remove(r.size() - 1);
+			}
+
+			// replace the domain name with tokenized items
+			tokens.addAll(1, r);
+			tokens.remove(0);
+
+			// iterate through the tokens and apply xml->java name algorithm
+			for (int i = 0; i < tokens.size(); i++) {
+
+				// get the token and remove illegal chars
+				String token = tokens.get(i);
+				token = removeIllegalIdentifierChars(token);
+
+				// this will check for reserved keywords
+				if (SourceVersion.isKeyword(token.toLowerCase())) {
+					token = '_' + token;
+				}
+
+				tokens.set(i, token.toLowerCase());
+			}
+
+			// concat all the pieces and return it
+			return combine(tokens, '.');
+		}
+
+		private static String removeIllegalIdentifierChars(String token) {
+			StringBuilder newToken = new StringBuilder(token.length() + 1); // max
+																			// expected
+																			// length
+			for (int i = 0; i < token.length(); i++) {
+				char c = token.charAt(i);
+				if (i == 0 && !Character.isJavaIdentifierStart(c)) { // c can't
+																		// be
+																		// used
+																		// as
+																		// FIRST
+																		// char
+					newToken.append('_');
+				}
+				if (!Character.isJavaIdentifierPart(c)) { // c can't be used
+					newToken.append('_');
+				} else {
+					newToken.append(c); // c is valid
+				}
+			}
+			return newToken.toString();
+		}
+
+		private static ArrayList<String> tokenize(String str, String sep) {
+			StringTokenizer tokens = new StringTokenizer(str, sep);
+			ArrayList<String> r = new ArrayList<String>();
+
+			while (tokens.hasMoreTokens())
+				r.add(tokens.nextToken());
+
+			return r;
+		}
+
+		private static <T> ArrayList<T> reverse(List<T> a) {
+			ArrayList<T> r = new ArrayList<T>();
+
+			for (int i = a.size() - 1; i >= 0; i--)
+				r.add(a.get(i));
+
+			return r;
+		}
+
+		private static String combine(List r, char sep) {
+			StringBuilder buf = new StringBuilder(r.get(0).toString());
+
+			for (int i = 1; i < r.size(); i++) {
+				buf.append(sep);
+				buf.append(r.get(i));
+			}
+
+			return buf.toString();
+		}
+	}
+
+	/**
+	 * JAX-PRC compatible name converter implementation.
+	 *
+	 * The only difference is that we treat '_' as a valid character and not as
+	 * a word separator.
+	 */
+	public static final NameConverter getJaxrpcCompatible() {
+		return new Standard() {
+			@Override
+			protected boolean isPunct(char c) {
+				return (c == '.' || c == '-' || c == ';' /*
+															 * || c == '_'
+															 */ || c == '\u00b7' || c == '\u0387' || c == '\u06dd' || c == '\u06de');
+			}
+
+			@Override
+			protected boolean isLetter(char c) {
+				return super.isLetter(c) || c == '_';
+			}
+
+			@Override
+			protected int classify(char c0) {
+				if (c0 == '_')
+					return NameUtil.OTHER_LETTER;
+				return super.classify(c0);
+			}
+		};
+	}
+
+	/**
+	 * Smarter converter used for RELAX NG support.
+	 */
+	public static final NameConverter getSmart() {
+		return new Standard() {
+			@Override
+			public String toConstantName(String token) {
+				String name = super.toConstantName(token);
+				if (!SourceVersion.isKeyword(name))
+					return name;
+				else
+					return '_' + name;
+			}
+		};
+	}
+
+}
