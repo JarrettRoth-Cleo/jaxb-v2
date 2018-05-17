e199fe4ce4ccf6df7cfe818e8822e0fff66e54a3
Jarrett Roth <jroth@cleo.com>
2018-05-17 17:06:09 -0400

    adding a way to delete the autogenerated file

--- jaxb-ri/xjc/src/test/java/xjcTests/AbstractXJCTest.java
+++ jaxb-ri/xjc/src/test/java/xjcTests/AbstractXJCTest.java
@@ -48,6 +48,8 @@ public class AbstractXJCTest {
 	public void tearDown() {
 		if (shouldCleanUpAfter) {
             FileUtils.delete(outputDir);
+			File f = new File("src/test/resources/genBindings.xjb");
+			FileUtils.delete(f);
 		}
 	}
 
