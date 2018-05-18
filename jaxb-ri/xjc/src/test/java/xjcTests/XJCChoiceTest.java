6b2456a09d3c826bfea27eb1b5fcd2fec3458a55
wgeiger <wgeiger@cleo.com>
2018-05-18 09:25:29 -0400

    Attempted to modified the choice caching logic and discovered issues with its casting logic. Committing current state

--- jaxb-ri/xjc/src/test/java/xjcTests/XJCChoiceTest.java
+++ jaxb-ri/xjc/src/test/java/xjcTests/XJCChoiceTest.java
@@ -43,19 +43,47 @@ public class XJCChoiceTest extends AbstractXJCTest {
 
 			@Override
 			protected void handleJCodeModel(JCodeModel jModel, File outputDir) throws IOException {
-				try {
-					manager.modify(jModel);
-				} catch (ModelModificationException e) {
-					Assert.fail();
-				}
-
-				if (!outputDir.exists()) {
-					outputDir.mkdirs();
-				}
-				jModel.build(outputDir);
+				runManager(jModel, outputDir, false);
 			}
 
 		});
+	}
+
+	@Test
+	// TODO: need to make this have nested classes like a pleb
+	public void overridingParentInterfaceTest() throws Throwable {
+		runTest(new Logic() {
+
+			@Override
+			protected File getXsd() {
+				return new File(resourceDir, "ChoiceExampleWithNameResolution.xsd");
+			}
+
+			@Override
+			protected void loadPlugins(List<Plugin> plugins) {
+				plugins.add(new ChoiceResolutionPlugin());
+			}
+
+			@Override
+			protected void handleJCodeModel(JCodeModel jModel, File outputDir) throws IOException {
+				runManager(jModel, outputDir, true);
+			}
+
+		});
+	}
+
+	private void runManager(JCodeModel jModel, File outputDir, boolean genCode) throws IOException {
+		try {
+			manager.modify(jModel);
+		} catch (ModelModificationException e) {
+			Assert.fail();
+		}
+		if (genCode) {
+			if (!outputDir.exists()) {
+				outputDir.mkdirs();
+			}
+			jModel.build(outputDir);
+		}
 	}
 
 	class ChoiceResolutionPlugin extends Plugin {
@@ -79,20 +107,21 @@ public class XJCChoiceTest extends AbstractXJCTest {
 		public void postProcessModel(Model model, ErrorHandler errorHandler) {
 			for (Map.Entry<NClass, CClassInfo> beanEntry : model.beans().entrySet()) {
 				// TODO: remove this hard coded value.
-				if ("issue.choice.just.clarify.cleo.ChoiceType".equals(beanEntry.getKey().toString())) {
-					CClassInfo info = beanEntry.getValue();
-					for (CPropertyInfo propInfo : info.getProperties()) {
-						ParticleImpl comp = (ParticleImpl) propInfo.getSchemaComponent();
-						ModelGroupImpl term = (ModelGroupImpl) comp.getTerm();
+				// if
+				// ("issue.choice.just.clarify.cleo.ChoiceType".equals(beanEntry.getKey().toString()))
+				// {
+				CClassInfo info = beanEntry.getValue();
+				for (CPropertyInfo propInfo : info.getProperties()) {
+					ParticleImpl comp = (ParticleImpl) propInfo.getSchemaComponent();
+					ModelGroupImpl term = (ModelGroupImpl) comp.getTerm();
 
-						if ("CHOICE".equals(term.getCompositor().name())) {
-							manager.addChoice(propInfo);
-						}
+					if ("CHOICE".equals(term.getCompositor().name())) {
+						manager.addChoice(propInfo);
 					}
+					// }
 				}
 			}
 		}
-
 	}
 
 }
