25ec69a2c7b9e1a8f2a06c7e32c7b56bc40cb9eb
wgeiger <wgeiger@cleo.com>
2018-05-30 11:58:57 -0400

    Added class that will hold a rename request

---
+++ jaxb-ri/xjc/src/test/java/xjcTests/temp/NameResolutionLogic.java
@@ -0,0 +1,10 @@
+package xjcTests.temp;
+
+/**
+ * Hopefully provide a container for a change that needs to be made during a
+ * retry for name resolution
+ *
+ */
+public class NameResolutionLogic {
+
+}
