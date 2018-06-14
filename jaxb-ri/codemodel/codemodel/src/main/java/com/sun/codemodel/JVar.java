38ad91efdb563e57243679e55a97eaa505cac3f1
wgeiger <wgeiger@cleo.com>
2018-06-14 16:03:22 -0400

    Made some more fields mutable in the code model

--- jaxb-ri/codemodel/codemodel/src/main/java/com/sun/codemodel/JVar.java
+++ jaxb-ri/codemodel/codemodel/src/main/java/com/sun/codemodel/JVar.java
@@ -42,10 +42,9 @@ package com.sun.codemodel;
 
 import java.lang.annotation.Annotation;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Collection;
 import java.util.Collections;
-
+import java.util.List;
 
 /**
  * Variables and fields.
@@ -53,183 +52,182 @@ import java.util.Collections;
 
 public class JVar extends JExpressionImpl implements JDeclaration, JAssignmentTarget, JAnnotatable {
 
-    /**
-     * Modifiers.
-     */
-    private JMods mods;
+	/**
+	 * Modifiers.
+	 */
+	private JMods mods;
 
-    /**
-     * JType of the variable
-     */
-    private JType type;
+	/**
+	 * JType of the variable
+	 */
+	private JType type;
 
-    /**
-     * Name of the variable
-     */
-    private String name;
+	/**
+	 * Name of the variable
+	 */
+	private String name;
 
-    /**
-     * Initialization of the variable in its declaration
-     */
-    private JExpression init;
+	/**
+	 * Initialization of the variable in its declaration
+	 */
+	private JExpression init;
 
-    /**
-     * Annotations on this variable. Lazily created.
-     */
-    private List<JAnnotationUse> annotations = null;
+	/**
+	 * Annotations on this variable. Lazily created.
+	 */
+	private List<JAnnotationUse> annotations = null;
 
+	/**
+	 * JVar constructor
+	 *
+	 * @param type
+	 *            Datatype of this variable
+	 *
+	 * @param name
+	 *            Name of this variable
+	 *
+	 * @param init
+	 *            Value to initialize this variable to
+	 */
+	JVar(JMods mods, JType type, String name, JExpression init) {
+		this.mods = mods;
+		this.type = type;
+		this.name = name;
+		this.init = init;
+	}
 
+	/**
+	 * Initialize this variable
+	 *
+	 * @param init
+	 *            JExpression to be used to initialize this field
+	 */
+	public JVar init(JExpression init) {
+		this.init = init;
+		return this;
+	}
 
-    /**
-     * JVar constructor
-     *
-     * @param type
-     *        Datatype of this variable
-     *
-     * @param name
-     *        Name of this variable
-     *
-     * @param init
-     *        Value to initialize this variable to
-     */
-    JVar(JMods mods, JType type, String name, JExpression init) {
-        this.mods = mods;
-        this.type = type;
-        this.name = name;
-        this.init = init;
-    }
+	/**
+	 * Get the name of this variable
+	 *
+	 * @return Name of the variable
+	 */
+	public String name() {
+		return name;
+	}
 
+	/**
+	 * Changes the name of this variable.
+	 */
+	public void name(String name) {
+		if (!JJavaName.isJavaIdentifier(name))
+			throw new IllegalArgumentException();
+		this.name = name;
+	}
 
-    /**
-     * Initialize this variable
-     *
-     * @param init
-     *        JExpression to be used to initialize this field
-     */
-    public JVar init(JExpression init) {
-        this.init = init;
-        return this;
-    }
+	/**
+	 * Return the type of this variable.
+	 * 
+	 * @return always non-null.
+	 */
+	public JType type() {
+		return type;
+	}
 
-    /**
-     * Get the name of this variable
-     *
-     * @return Name of the variable
-     */
-    public String name() {
-        return name;
-    }
+	/**
+	 * @return the current modifiers of this method. Always return non-null
+	 *         valid object.
+	 */
+	public JMods mods() {
+		return mods;
+	}
 
-    /**
-     * Changes the name of this variable.
-     */
-    public void name(String name) {
-        if(!JJavaName.isJavaIdentifier(name))
-            throw new IllegalArgumentException();
-        this.name = name;
-    }
+	/**
+	 * Sets the type of this variable.
+	 *
+	 * @param newType
+	 *            must not be null.
+	 *
+	 * @return the old type value. always non-null.
+	 */
+	public JType type(JType newType) {
+		JType r = type;
+		if (newType == null)
+			throw new IllegalArgumentException();
+		type = newType;
+		return r;
+	}
 
-    /**
-     * Return the type of this variable.
-     * @return
-     *      always non-null.
-     */
-    public JType type() {
-        return type;
-    }
+	/**
+	 * Adds an annotation to this variable.
+	 * 
+	 * @param clazz
+	 *            The annotation class to annotate the field with
+	 */
+	public JAnnotationUse annotate(JClass clazz) {
+		if (annotations == null)
+			annotations = new ArrayList<JAnnotationUse>();
+		JAnnotationUse a = new JAnnotationUse(clazz);
+		annotations.add(a);
+		return a;
+	}
 
-    /**
-     * @return
-     *      the current modifiers of this method.
-     *      Always return non-null valid object.
-     */
-    public JMods mods() {
-        return mods;
-    }
+	/**
+	 * Adds an annotation to this variable.
+	 *
+	 * @param clazz
+	 *            The annotation class to annotate the field with
+	 */
+	public JAnnotationUse annotate(Class<? extends Annotation> clazz) {
+		return annotate(type.owner().ref(clazz));
+	}
 
-    /**
-     * Sets the type of this variable.
-     *
-     * @param newType
-     *      must not be null.
-     *
-     * @return
-     *      the old type value. always non-null.
-     */
-    public JType type(JType newType) {
-        JType r = type;
-        if(newType==null)
-            throw new IllegalArgumentException();
-        type = newType;
-        return r;
-    }
+	public <W extends JAnnotationWriter> W annotate2(Class<W> clazz) {
+		return TypedAnnotationWriter.create(clazz, this);
+	}
 
+	public boolean removeAnnotation(JAnnotationUse annotation) {
+		return this.annotations.remove(annotation);
+	}
 
-    /**
-     * Adds an annotation to this variable.
-     * @param clazz
-     *          The annotation class to annotate the field with
-     */
-    public JAnnotationUse annotate(JClass clazz){
-        if(annotations==null)
-           annotations = new ArrayList<JAnnotationUse>();
-        JAnnotationUse a = new JAnnotationUse(clazz);
-        annotations.add(a);
-        return a;
-    }
+	public Collection<JAnnotationUse> annotations() {
+		if (annotations == null)
+			annotations = new ArrayList<JAnnotationUse>();
+		return Collections.unmodifiableList(annotations);
+	}
 
-    /**
-     * Adds an annotation to this variable.
-     *
-     * @param clazz
-     *          The annotation class to annotate the field with
-     */
-    public JAnnotationUse annotate(Class <? extends Annotation> clazz){
-        return annotate(type.owner().ref(clazz));
-    }
+	protected boolean isAnnotated() {
+		return annotations != null;
+	}
 
-    public <W extends JAnnotationWriter> W annotate2(Class<W> clazz) {
-        return TypedAnnotationWriter.create(clazz,this);
-    }
+	public void bind(JFormatter f) {
+		if (annotations != null) {
+			for (int i = 0; i < annotations.size(); i++)
+				f.g(annotations.get(i)).nl();
+		}
+		f.g(mods).g(type).id(name);
+		if (init != null)
+			f.p('=').g(init);
+	}
 
-    public boolean removeAnnotation(JAnnotationUse annotation) {
-        return this.annotations.remove(annotation);
-    }
+	public void declare(JFormatter f) {
+		f.b(this).p(';').nl();
+	}
 
-    public Collection<JAnnotationUse> annotations() {
-        if (annotations == null)
-            annotations = new ArrayList<JAnnotationUse>();
-        return Collections.unmodifiableList(annotations);
-    }
+	public void generate(JFormatter f) {
+		f.id(name);
+	}
 
-    protected boolean isAnnotated() {
-        return annotations!=null;
-    }
+	public JExpression assign(JExpression rhs) {
+		return JExpr.assign(this, rhs);
+	}
 
-    public void bind(JFormatter f) {
-        if (annotations != null){
-            for( int i=0; i<annotations.size(); i++ )
-                f.g(annotations.get(i)).nl();
-        }
-        f.g(mods).g(type).id(name);
-        if (init != null)
-            f.p('=').g(init);
-    }
+	public JExpression assignPlus(JExpression rhs) {
+		return JExpr.assignPlus(this, rhs);
+	}
 
-    public void declare(JFormatter f) {
-        f.b(this).p(';').nl();
-    }
+	public void mods(JMods mods) {
+		this.mods = mods;
+	}
 
-    public void generate(JFormatter f) {
-        f.id(name);
-    }
-
-	
-    public JExpression assign(JExpression rhs) {
-		return JExpr.assign(this,rhs);
-    }
-    public JExpression assignPlus(JExpression rhs) {
-		return JExpr.assignPlus(this,rhs);
-    }
-	
 }
