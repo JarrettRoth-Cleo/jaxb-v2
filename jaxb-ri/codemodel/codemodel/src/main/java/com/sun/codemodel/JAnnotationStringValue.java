bb18261a2debcb260dac7a40c89e8be781929140
wgeiger <wgeiger@cleo.com>
2018-06-14 15:05:12 -0400

    Exposed the JAnnotationStringValue and its value method

--- jaxb-ri/codemodel/codemodel/src/main/java/com/sun/codemodel/JAnnotationStringValue.java
+++ jaxb-ri/codemodel/codemodel/src/main/java/com/sun/codemodel/JAnnotationStringValue.java
@@ -40,26 +40,27 @@
 
 package com.sun.codemodel;
 
-
-
 /**
  * Captures the value of the annotation.
  *
- * @author
- *     Bhakti Mehta (bhakti.mehta@sun.com)
+ * @author Bhakti Mehta (bhakti.mehta@sun.com)
  */
-final class JAnnotationStringValue extends JAnnotationValue {
+public final class JAnnotationStringValue extends JAnnotationValue {
 
-    /**
-     * The value of the Annotation member
-     */
-    private final JExpression value;
+	/**
+	 * The value of the Annotation member
+	 */
+	private final JExpression value;
 
-    JAnnotationStringValue(JExpression value) {
-        this.value = value;
-    }
+	JAnnotationStringValue(JExpression value) {
+		this.value = value;
+	}
 
-    public void generate(JFormatter f) {
-        f.g(value);
-    }
+	public void generate(JFormatter f) {
+		f.g(value);
+	}
+
+	public JExpression getValue() {
+		return value;
+	}
 }
