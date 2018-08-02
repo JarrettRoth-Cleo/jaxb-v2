8a2640c17d8166b07815908c062010501e51a8fa
wgeiger <wgeiger@cleo.com>
2018-08-02 10:46:14 -0400

    CElementInfo is checking to see if the source object has a binding before setting the squeezedNameHelper

--- jaxb-ri/xjc/src/main/java/com/sun/tools/xjc/model/CElementInfo.java
+++ jaxb-ri/xjc/src/main/java/com/sun/tools/xjc/model/CElementInfo.java
@@ -64,271 +64,299 @@ import com.sun.tools.xjc.model.nav.NavigatorImpl;
 import com.sun.tools.xjc.outline.Outline;
 import com.sun.tools.xjc.reader.Ring;
 import com.sun.tools.xjc.reader.xmlschema.BGMBuilder;
+import com.sun.tools.xjc.reader.xmlschema.bindinfo.BIDeclaration;
 import com.sun.tools.xjc.reader.xmlschema.bindinfo.BIFactoryMethod;
 import com.sun.tools.xjc.reader.xmlschema.bindinfo.BIInlineBinaryData;
+import com.sun.tools.xjc.reader.xmlschema.bindinfo.BIProperty;
+import com.sun.tools.xjc.reader.xmlschema.bindinfo.BindInfo;
 import com.sun.xml.bind.v2.model.core.ElementInfo;
 import com.sun.xml.xsom.XSElementDecl;
 import com.sun.xml.xsom.XmlString;
+import com.sun.xml.xsom.impl.AnnotationImpl;
+import com.sun.xml.xsom.impl.ElementDecl;
 
 /**
  * {@link ElementInfo} implementation for the compile-time model.
  *
  * <p>
- * As an NType, it represents the Java representation of this element
- * (either JAXBElement&lt;T> or Foo).
+ * As an NType, it represents the Java representation of this element (either
+ * JAXBElement&lt;T> or Foo).
  *
  * @author Kohsuke Kawaguchi
  */
-public final class CElementInfo extends AbstractCElement
-    implements ElementInfo<NType,NClass>, NType, CClassInfoParent {
+public final class CElementInfo extends AbstractCElement implements ElementInfo<NType, NClass>, NType, CClassInfoParent {
 
-    private final QName tagName;
+	private final QName tagName;
 
-    /**
-     * Represents {@code JAXBElement&lt;ContentType>}.
-     */
-    private NType type;
+	/**
+	 * Represents {@code JAXBElement&lt;ContentType>}.
+	 */
+	private NType type;
 
-    /**
-     * If this element produces its own class, the short name of that class.
-     * Otherwise null.
-     */
-    private String className;
+	/**
+	 * If this element produces its own class, the short name of that class.
+	 * Otherwise null.
+	 */
+	private String className;
 
-    /**
-     * If this element is global, the element info is considered to be
-     * package-level, and this points to the package in which this element
-     * lives in.
-     *
-     * <p>
-     * For local elements, this points to the parent {@link CClassInfo}.
-     */
-    public final CClassInfoParent parent;
+	/**
+	 * If this element is global, the element info is considered to be
+	 * package-level, and this points to the package in which this element lives
+	 * in.
+	 *
+	 * <p>
+	 * For local elements, this points to the parent {@link CClassInfo}.
+	 */
+	public final CClassInfoParent parent;
 
-    private CElementInfo substitutionHead;
+	private CElementInfo substitutionHead;
 
-    /**
-     * Lazily computed.
-     */
-    private Set<CElementInfo> substitutionMembers;
+	/**
+	 * Lazily computed.
+	 */
+	private Set<CElementInfo> substitutionMembers;
 
-    /**
-     * {@link Model} that owns this object.
-     */
-    private final Model model;
+	/**
+	 * {@link Model} that owns this object.
+	 */
+	private final Model model;
 
-    private CElementPropertyInfo property;
+	private CElementPropertyInfo property;
 
-    /**
-     * Custom {@link #getSqueezedName() squeezed name}, if any.
-     */
-    private /*almost final*/ @Nullable String squeezedName;
+	/**
+	 * Custom {@link #getSqueezedName() squeezed name}, if any.
+	 */
+	private /* almost final */ @Nullable String squeezedName;
 
-    private @Nullable String squeezedNameHelper;
-    
-    /**
-     * Creates an element in the given parent.
-     *
-     * <p>
-     * When using this construction, {@link #initContentType(TypeUse, XSElementDecl, XmlString)}
-     * must not be invoked.
-     */
-    public CElementInfo(Model model,QName tagName, CClassInfoParent parent, TypeUse contentType, XmlString defaultValue, XSElementDecl source, CCustomizations customizations, Locator location ) {
-        super(model,source,location,customizations);
-        this.tagName = tagName;
-        this.model = model;
-        this.parent = parent;
-        if(contentType!=null)
-            initContentType(contentType, source, defaultValue);
-        
-        if(contentType instanceof CClassInfo){
-        	String[] contentTypeStr = contentType.toString().split("\\.");
-        	squeezedNameHelper = contentTypeStr[contentTypeStr.length-1];
-        }
-        model.add(this);
-    }
+	private @Nullable String squeezedNameHelper;
 
-    /**
-     * Creates an element with a class in the given parent.
-     *
-     * <p>
-     * When using this construction, the caller must use
-     * {@link #initContentType(TypeUse, XSElementDecl, XmlString)} to fill in the content type
-     * later.
-     *
-     * This is to avoid a circular model construction dependency between buidling a type
-     * inside an element and element itself. To build a content type, you need to have
-     * {@link CElementInfo} for a parent, so we can't take it as a constructor parameter.
-     */
-    public CElementInfo(Model model,QName tagName, CClassInfoParent parent, String className, CCustomizations customizations, Locator location ) {
-        this(model,tagName,parent,null,null,null,customizations,location);
-        this.className = className;
-    }
+	/**
+	 * Creates an element in the given parent.
+	 *
+	 * <p>
+	 * When using this construction,
+	 * {@link #initContentType(TypeUse, XSElementDecl, XmlString)} must not be
+	 * invoked.
+	 */
+	public CElementInfo(Model model, QName tagName, CClassInfoParent parent, TypeUse contentType, XmlString defaultValue, XSElementDecl source,
+			CCustomizations customizations, Locator location) {
+		super(model, source, location, customizations);
+		this.tagName = tagName;
+		this.model = model;
+		this.parent = parent;
+		if (contentType != null)
+			initContentType(contentType, source, defaultValue);
 
-    public void initContentType(TypeUse contentType, @Nullable XSElementDecl source, XmlString defaultValue) {
-        assert this.property==null; // must not be called twice
+		setSqueezedNameHelper(contentType, source);
+		model.add(this);
+	}
 
-        this.property = new CElementPropertyInfo("Value",
-                contentType.isCollection()?REPEATED_VALUE:NOT_REPEATED,
-                contentType.idUse(),
-                contentType.getExpectedMimeType(),
-                source,null,getLocator(),true);
-        this.property.setAdapter(contentType.getAdapterUse());
-        BIInlineBinaryData.handle(source,property);
-        property.getTypes().add(new CTypeRef(contentType.getInfo(),tagName,CTypeRef.getSimpleTypeName(source), true,defaultValue));
-        this.type = NavigatorImpl.createParameterizedType(
-            NavigatorImpl.theInstance.ref(JAXBElement.class),
-            getContentInMemoryType() );
+	/**
+	 * Set the squeezed name helper if the source definition had a binding added
+	 * to it
+	 * 
+	 * @param contentType
+	 * @param source
+	 */
+	private void setSqueezedNameHelper(TypeUse contentType, XSElementDecl source) {
+		if (contentType instanceof CClassInfo && source instanceof ElementDecl) {
+			AnnotationImpl annImpl = ((AnnotationImpl) ((ElementDecl) source).getAnnotation());
+			if (annImpl != null) {
+				Object o = annImpl.getAnnotation();
+				if (o instanceof BindInfo) {
+					BindInfo info = (BindInfo) o;
+					for (BIDeclaration biDecl : info.getDecls()) {
+						if (biDecl instanceof BIProperty) {
+							String[] contentTypeStr = contentType.toString().split("\\.");
+							squeezedNameHelper = contentTypeStr[contentTypeStr.length - 1];
+							return;
+						}
+					}
+				}
+			}
+		}
+	}
 
-        BIFactoryMethod factoryMethod = Ring.get(BGMBuilder.class).getBindInfo(source).get(BIFactoryMethod.class);
-        if(factoryMethod!=null) {
-            factoryMethod.markAsAcknowledged();
-            this.squeezedName = factoryMethod.name;
-        }
+	/**
+	 * Creates an element with a class in the given parent.
+	 *
+	 * <p>
+	 * When using this construction, the caller must use
+	 * {@link #initContentType(TypeUse, XSElementDecl, XmlString)} to fill in
+	 * the content type later.
+	 *
+	 * This is to avoid a circular model construction dependency between
+	 * buidling a type inside an element and element itself. To build a content
+	 * type, you need to have {@link CElementInfo} for a parent, so we can't
+	 * take it as a constructor parameter.
+	 */
+	public CElementInfo(Model model, QName tagName, CClassInfoParent parent, String className, CCustomizations customizations, Locator location) {
+		this(model, tagName, parent, null, null, null, customizations, location);
+		this.className = className;
+	}
 
-    }
+	public void initContentType(TypeUse contentType, @Nullable XSElementDecl source, XmlString defaultValue) {
+		assert this.property == null; // must not be called twice
 
-    public final String getDefaultValue() {
-        return getProperty().getTypes().get(0).getDefaultValue();
-    }
+		this.property = new CElementPropertyInfo("Value", contentType.isCollection() ? REPEATED_VALUE : NOT_REPEATED, contentType.idUse(),
+				contentType.getExpectedMimeType(), source, null, getLocator(), true);
+		this.property.setAdapter(contentType.getAdapterUse());
+		BIInlineBinaryData.handle(source, property);
+		property.getTypes().add(new CTypeRef(contentType.getInfo(), tagName, CTypeRef.getSimpleTypeName(source), true, defaultValue));
+		this.type = NavigatorImpl.createParameterizedType(NavigatorImpl.theInstance.ref(JAXBElement.class), getContentInMemoryType());
 
-    public final JPackage _package() {
-        return parent.getOwnerPackage();
-    }
+		BIFactoryMethod factoryMethod = Ring.get(BGMBuilder.class).getBindInfo(source).get(BIFactoryMethod.class);
+		if (factoryMethod != null) {
+			factoryMethod.markAsAcknowledged();
+			this.squeezedName = factoryMethod.name;
+		}
 
-    @Override
+	}
+
+	public final String getDefaultValue() {
+		return getProperty().getTypes().get(0).getDefaultValue();
+	}
+
+	public final JPackage _package() {
+		return parent.getOwnerPackage();
+	}
+
+	@Override
 	public CNonElement getContentType() {
-        return getProperty().ref().get(0);
-    }
+		return getProperty().ref().get(0);
+	}
 
-    @Override
+	@Override
 	public NType getContentInMemoryType() {
-        if(getProperty().getAdapter()==null) {
-            NType itemType = getContentType().getType();
-            if(!property.isCollection())
-                return itemType;
+		if (getProperty().getAdapter() == null) {
+			NType itemType = getContentType().getType();
+			if (!property.isCollection())
+				return itemType;
 
-            return NavigatorImpl.createParameterizedType(List.class,itemType);
-        } else {
-            return getProperty().getAdapter().customType;
-        }
-    }
+			return NavigatorImpl.createParameterizedType(List.class, itemType);
+		} else {
+			return getProperty().getAdapter().customType;
+		}
+	}
 
-    @Override
+	@Override
 	public CElementPropertyInfo getProperty() {
-        return property;
-    }
+		return property;
+	}
 
-    @Override
+	@Override
 	public CClassInfo getScope() {
-        if(parent instanceof CClassInfo)
-            return (CClassInfo)parent;
-        return null;
-    }
+		if (parent instanceof CClassInfo)
+			return (CClassInfo) parent;
+		return null;
+	}
 
-    /**
-     * @deprecated why are you calling a method that returns this?
-     */
-    @Deprecated
+	/**
+	 * @deprecated why are you calling a method that returns this?
+	 */
+	@Deprecated
 	@Override
 	public NType getType() {
-        return this;
-    }
+		return this;
+	}
 
-    @Override
+	@Override
 	public QName getElementName() {
-        return tagName;
-    }
+		return tagName;
+	}
 
-    @Override
+	@Override
 	public JType toType(Outline o, Aspect aspect) {
-        if(className==null)
-            return type.toType(o,aspect);
-        else
-            return o.getElement(this).implClass;
-    }
+		if (className == null)
+			return type.toType(o, aspect);
+		else
+			return o.getElement(this).implClass;
+	}
 
-    /**
-     * Returns the "squeezed name" of this element.
-     *
-     * @see CClassInfo#getSqueezedName()
-     */
-    @XmlElement
-    public String getSqueezedName() {
-        if(squeezedName!=null)  return squeezedName;
-        
-        StringBuilder b = new StringBuilder();
-        CClassInfo s = getScope();
-        if(s!=null)
-            b.append(s.getSqueezedName());
-        if(className!=null)
-            b.append(className);
-        else if(squeezedNameHelper!=null)
-        	b.append( model.getNameConverter().toClassName(squeezedNameHelper));
-        else
-        	b.append( model.getNameConverter().toClassName(tagName.getLocalPart()));
-        return b.toString();
-    }
+	/**
+	 * Returns the "squeezed name" of this element.
+	 *
+	 * @see CClassInfo#getSqueezedName()
+	 */
+	@XmlElement
+	public String getSqueezedName() {
+		if (squeezedName != null)
+			return squeezedName;
 
-    @Override
+		StringBuilder b = new StringBuilder();
+		CClassInfo s = getScope();
+		if (s != null)
+			b.append(s.getSqueezedName());
+		if (className != null)
+			b.append(className);
+		else if (squeezedNameHelper != null)
+			b.append(model.getNameConverter().toClassName(squeezedNameHelper));
+		else
+			b.append(model.getNameConverter().toClassName(tagName.getLocalPart()));
+		return b.toString();
+	}
+
+	@Override
 	public CElementInfo getSubstitutionHead() {
-        return substitutionHead;
-    }
+		return substitutionHead;
+	}
 
-    @Override
+	@Override
 	public Collection<CElementInfo> getSubstitutionMembers() {
-        if(substitutionMembers==null)
-            return Collections.emptyList();
-        else
-            return substitutionMembers;
-    }
+		if (substitutionMembers == null)
+			return Collections.emptyList();
+		else
+			return substitutionMembers;
+	}
 
-    public void setSubstitutionHead(CElementInfo substitutionHead) {
-        // don't set it twice
-        assert this.substitutionHead==null;
-        assert substitutionHead!=null;
-        this.substitutionHead = substitutionHead;
+	public void setSubstitutionHead(CElementInfo substitutionHead) {
+		// don't set it twice
+		assert this.substitutionHead == null;
+		assert substitutionHead != null;
+		this.substitutionHead = substitutionHead;
 
-        if(substitutionHead.substitutionMembers==null)
-            substitutionHead.substitutionMembers = new HashSet<CElementInfo>();
-        substitutionHead.substitutionMembers.add(this);
-    }
+		if (substitutionHead.substitutionMembers == null)
+			substitutionHead.substitutionMembers = new HashSet<CElementInfo>();
+		substitutionHead.substitutionMembers.add(this);
+	}
 
-    @Override
+	@Override
 	public boolean isBoxedType() {
-        return false;
-    }
+		return false;
+	}
 
-    @Override
+	@Override
 	public String fullName() {
-        if(className==null)
-            return type.fullName();
-        else {
-            String r = parent.fullName();
-            if(r.length()==0)   return className;
-            else                return r+'.'+className;
-        }
-    }
+		if (className == null)
+			return type.fullName();
+		else {
+			String r = parent.fullName();
+			if (r.length() == 0)
+				return className;
+			else
+				return r + '.' + className;
+		}
+	}
 
-    @Override
+	@Override
 	public <T> T accept(Visitor<T> visitor) {
-        return visitor.onElement(this);
-    }
+		return visitor.onElement(this);
+	}
 
-    @Override
+	@Override
 	public JPackage getOwnerPackage() {
-        return parent.getOwnerPackage();
-    }
+		return parent.getOwnerPackage();
+	}
 
-    public String shortName() {
-        return className;
-    }
+	public String shortName() {
+		return className;
+	}
 
-    /**
-     * True if this element has its own class
-     * (as opposed to be represented as an instance of {@link JAXBElement}.
-     */
-    public boolean hasClass() {
-        return className!=null;
-    }
+	/**
+	 * True if this element has its own class (as opposed to be represented as
+	 * an instance of {@link JAXBElement}.
+	 */
+	public boolean hasClass() {
+		return className != null;
+	}
 }
